275cfe2a8e3be8d756e4bbb847521b2fba59c922
Jordan Hall <jordan@libertyware.co.uk>
2024-02-10 13:55:55 +0000

    feat: zone bypass to turn on provideClientHydration. Without zone provideClientHydration does nothing by error

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,34 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+type Zone = {
+  current: {
+    get(key: string): boolean | undefined;
+    run<T>(fn: (...args: any[]) => T, applyThis?: any, applyArgs?: any): T;
+  };
+  run<T>(fn: (...args: any[]) => T, applyThis?: any, applyArgs?: any): T ;
+};
+declare var Zone:  Zone
+declare module globalThis {
+	let Zone: Zone
+}
+
+if (typeof Zone === 'undefined') {
+  const mockZone = {
+    current: {
+      
+      get: (key: string): boolean | undefined => key === 'isAngularZone' ? true : undefined,
+      run<T>(fn: (...args: any[]) => T, applyThis?: any, applyArgs?: any): T {
+        return fn.apply(applyThis, applyArgs);
+      }
+    },
+    run<T>(fn: (...args: any[]) => T, applyThis?: any, applyArgs?: any): T {
+      return fn.apply(applyThis, applyArgs);
+    }
+  };
+  globalThis.Zone = mockZone;
+}
+
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
