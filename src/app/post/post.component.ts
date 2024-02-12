667fa32cf09301abe05b9c0205ad28db51a30f7a
sp90 <dev@nomis.dk>
2024-02-12 15:06:25 +0100

    Add old input and fix param types

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,25 +1,46 @@
 import { JsonPipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import {
   ChangeDetectionStrategy,
   Component,
+  Input,
   inject,
-  input} from '@angular/core';
+  input,
+} from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
 import { ActivatedRouteSnapshot } from '@angular/router';
-import { ApiService } from '../api.service';
 import { map } from 'rxjs';
+import { ApiService } from '../api.service';
 
 const fb = new FormBuilder();
 export const resolvePost = {
   post: (route: ActivatedRouteSnapshot) => {
-    const res = inject(ApiService).client.id[route.params['id']].get({}).pipe(map(test=> {
-      console.log(' ');
-      console.log('Post resolver', test);
-      console.log(' ');
-  
-      return test.data;
-    }))
+    const res = inject(ApiService)
+      .client.id[route.params['id']].get()
+      .pipe(
+        map((test) => {
+          console.log(' ');
+          console.log('Post resolver', test);
+          console.log(' ');
+
+          return test.data;
+        })
+      );
+
+    return res;
+  },
+  postOldInput: (route: ActivatedRouteSnapshot) => {
+    const res = inject(ApiService)
+      .client.id[route.params['id']].get()
+      .pipe(
+        map((test) => {
+          console.log(' ');
+          console.log('Post resolver', test);
+          console.log(' ');
+
+          return test.data;
+        })
+      );
 
     return res;
   },
@@ -31,12 +52,8 @@ export const resolvePost = {
   imports: [JsonPipe, ReactiveFormsModule],
   template: `
     <h1>Post</h1>
+    {{ postOldInput }}
     {{ post() }}
-    @if (post !== null) {
-      {{ post() }}
-    } @else {
-      <p>Loading....</p>
-    }
 
     <form [formGroup]="form" (submit)="submit()">
       <input type="text" formControlName="strField" />
@@ -47,13 +64,12 @@ export const resolvePost = {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class PostComponent {
-  private api = inject(HttpClient)
+  private api = inject(HttpClient);
 
   post = input();
-  
+  @Input('postOldInput') postOldInput!: string;
 
   title = 'treaty';
-
   form = fb.group({
     strField: fb.control('', Validators.required),
     numbField: fb.control<number | null>(null),
@@ -62,11 +78,14 @@ export default class PostComponent {
   async submit() {
     if (this.form.invalid) return;
 
-    const res = this.api.post('/api/form',
-      this.form.value as {
-        strField: string;
-        numbField: number;
-      }
-    ).subscribe(res => console.log('res: ', res));
+    const res = this.api
+      .post(
+        '/api/form',
+        this.form.value as {
+          strField: string;
+          numbField: number;
+        }
+      )
+      .subscribe((res) => console.log('res: ', res));
   }
 }
