52c361b60ec4e95ea296f7622252dea6b30600c4
Jordan Hall <jordan@libertyware.co.uk>
2024-09-26 21:13:59 +0100

    chore: migrate repo to moon tool out of principle

---
+++ language-tool/language-server/src/index.ts
@@ -0,0 +1,72 @@
+import { createConnection, createServer, createTypeScriptProject, Diagnostic, loadTsdkByPath } from '@volar/language-server/node';
+import { create as createCssService } from 'volar-service-css';
+import { create as createEmmetService } from 'volar-service-emmet';
+import { create as createHtmlService } from 'volar-service-html';
+import { create as createTypeScriptServices } from 'volar-service-typescript';
+import { URI } from 'vscode-uri';
+import { TreatyVirtualCode, treatyLanguagePlugin } from './languagePlugin';
+
+const connection = createConnection();
+const server = createServer(connection);
+
+connection.listen();
+
+connection.onInitialize(params => {
+	const tsdk = loadTsdkByPath(params.initializationOptions.typescript.tsdk, params.locale);
+	return server.initialize(
+		params,
+		createTypeScriptProject(tsdk.typescript, tsdk.diagnosticMessages, () => ({
+			languagePlugins: [treatyLanguagePlugin],
+		})),
+		[
+			createHtmlService(),
+			createCssService(),
+			createEmmetService(),
+			...createTypeScriptServices(tsdk.typescript),
+			{
+				capabilities: {
+					diagnosticProvider: {
+						interFileDependencies: false,
+						workspaceDiagnostics: false,
+					},
+				},
+				create(context) {
+					return {
+						provideDiagnostics(document) {
+							const decoded = context.decodeEmbeddedDocumentUri(URI.parse(document.uri));
+							if (!decoded) {
+								// Not a embedded document
+								return;
+							}
+							const virtualCode = context.language.scripts.get(decoded[0])?.generated?.embeddedCodes.get(decoded[1]);
+							if (!(virtualCode instanceof TreatyVirtualCode)) {
+								return;
+							}
+							const styleNodes = virtualCode.htmlDocument.roots.filter(root => root.tag === 'style');
+							if (styleNodes.length <= 1) {
+								return;
+							}
+							const errors: Diagnostic[] = [];
+							for (let i = 1; i < styleNodes.length; i++) {
+								errors.push({
+									severity: 2,
+									range: {
+										start: document.positionAt(styleNodes[i].start),
+										end: document.positionAt(styleNodes[i].end),
+									},
+									source: 'treaty',
+									message: 'Only one style tag is allowed.',
+								});
+							}
+							return errors;
+						},
+					};
+				},
+			},
+		],
+	)
+});
+
+connection.onInitialized(server.initialized);
+
+connection.onShutdown(server.shutdown);
